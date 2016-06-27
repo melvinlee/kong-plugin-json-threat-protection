cf517123751d050a3022ead7ffb92ddb69e9b5e3
Jonas Vekeman <jonas@trust1team.com>
2016-06-27 16:57:16 +0200

    Removed source from config

--- src/schema.lua
+++ src/schema.lua
@@ -5,7 +5,6 @@ return {
         container_depth = { required = false, type = "number", default = 0 },
         object_entry_count = { required = false, type = "number", default = 0 },
         object_entry_name_length = { required = false, type = "number", default = 0 },
-        source = { required = false, type = "string", enum = {"request", "response", "message"}, default = "request" },
         string_value_length = { required = false, type = "number", default = 0 }
     }
 }
--- test/plugins/json-threat-protection/schema_spec.lua
+++ test/plugins/json-threat-protection/schema_spec.lua
@@ -13,12 +13,11 @@ describe("JSON Threat Protection schema", function()
         assert.is_true(config.container_depth == 0)
         assert.is_true(config.object_entry_count == 0)
         assert.is_true(config.object_entry_name_length == 0)
-        assert.is_true(config.source == "request")
         assert.is_true(config.string_value_length == 0)
     end)
 
     it("should work when array element count is not set", function()
-        local config = { container_depth = 10, object_entry_count = 15, object_entry_name_length = 15, source = "request", string_value_length = 100 }
+        local config = { container_depth = 10, object_entry_count = 15, object_entry_name_length = 15, string_value_length = 100 }
         local valid, err = validate_entity(config, schema)
         assert.truthy(valid)
         assert.falsy(err)
@@ -26,7 +25,7 @@ describe("JSON Threat Protection schema", function()
     end)
 
     it("should work when container depth is not set", function()
-        local config = { array_element_count = 10, object_entry_count = 15, object_entry_name_length = 15, source = "request", string_value_length = 100 }
+        local config = { array_element_count = 10, object_entry_count = 15, object_entry_name_length = 15, string_value_length = 100 }
         local valid, err = validate_entity(config, schema)
         assert.truthy(valid)
         assert.falsy(err)
@@ -34,7 +33,7 @@ describe("JSON Threat Protection schema", function()
     end)
 
     it("should work when object entry count is not set", function()
-        local config = { array_element_count = 10, container_depth = 15, object_entry_name_length = 15, source = "request", string_value_length = 100 }
+        local config = { array_element_count = 10, container_depth = 15, object_entry_name_length = 15, string_value_length = 100 }
         local valid, err = validate_entity(config, schema)
         assert.truthy(valid)
         assert.falsy(err)
@@ -42,30 +41,15 @@ describe("JSON Threat Protection schema", function()
     end)
 
     it("should work when object entry entry name length is not set", function()
-        local config = { array_element_count = 10, container_depth = 15, object_entry_count = 15, source = "request", string_value_length = 100 }
+        local config = { array_element_count = 10, container_depth = 15, object_entry_count = 15, string_value_length = 100 }
         local valid, err = validate_entity(config, schema)
         assert.truthy(valid)
         assert.falsy(err)
         assert.is_true(config.object_entry_name_length == 0)
     end)
 
-    it("should work when source is not set", function()
-        local config = { array_element_count = 10, container_depth = 15, object_entry_count = 15, object_entry_name_length = 10, string_value_length = 100 }
-        local valid, err = validate_entity(config, schema)
-        assert.truthy(valid)
-        assert.falsy(err)
-        assert.is_true(config.source == "request")
-    end)
-
-    it("should not work when source has an invalid value", function()
-        local config = { source = "unknown" }
-        local valid, err = validate_entity(config, schema)
-        assert.falsy(valid)
-        assert.truthy(err)
-    end)
-
     it("should work when string value length is not set", function()
-        local config = { array_element_count = 10, container_depth = 15, object_entry_count = 15, object_entry_name_length = 10, source = "request" }
+        local config = { array_element_count = 10, container_depth = 15, object_entry_count = 15, object_entry_name_length = 10 }
         local valid, err = validate_entity(config, schema)
         assert.truthy(valid)
         assert.falsy(err)
